565ab34eece67e22b4449913f86a4f29bcc5d958
Ian <ian.moreno@artie.com>
2022-11-10 16:05:38 -0500

    Adding tests to the signature service

--- signing/interfaces.go
+++ signing/interfaces.go
@@ -1,13 +1,13 @@
 package signing
 
 import (
-	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 type SigningService interface {
 	SignTxn(signingKey string, args [][]byte) (string, string, error)
-	VerifyFromHash(signature []byte, hash common.Hash, key *ecdsa.PrivateKey) error
+	SignMessage(signingKey, message string) (string, string, error)
+	VerifyFromHash(signature []byte, hash common.Hash, address string) error
 	GenerateKey() (privateKey, address string, err error)
 	PrivateKeyToAddress(privateKey string) (address string, err error)
 	AddressToEIP55(address string) (string, error)
--- signing/signature_service.go
+++ signing/signature_service.go
@@ -29,6 +29,25 @@ func (s *SignatureHandler) WrapMessage(message string) common.Hash {
 	return crypto.Keccak256Hash([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(message), message)))
 }
 
+func (s *SignatureHandler) SignMessage(signingKey, message string) (string, string, error) {
+	key, keyParseErr := crypto.HexToECDSA(signingKey)
+	if keyParseErr != nil {
+		fmt.Println(keyParseErr.Error())
+		return "", "", keyParseErr
+	}
+	hash := s.WrapMessage(message)
+	signature, signErr := crypto.Sign(hash.Bytes(), key)
+	if signErr != nil {
+		fmt.Println(signErr.Error())
+		return "", "", signErr
+	}
+	if signature[crypto.RecoveryIDOffset] == 0 || signature[crypto.RecoveryIDOffset] == 1 {
+		signature[crypto.RecoveryIDOffset] += 27 // Transform yellow paper V from 0/1 to 27/28
+	}
+
+	return "0x"+hex.EncodeToString(hash.Bytes()), "0x"+hex.EncodeToString(signature), nil
+}
+
 func (s *SignatureHandler) SignTxn(signingKey string, args [][]byte) (string, string, error) {
 	hash := s.WrapHash(crypto.Keccak256Hash(args...))
 
@@ -51,7 +70,7 @@ func (s *SignatureHandler) SignTxn(signingKey string, args [][]byte) (string, st
 	return "0x"+hex.EncodeToString(hash.Bytes()), "0x"+hex.EncodeToString(signature), nil
 }
 
-func (s *SignatureHandler) VerifyFromHash(signature []byte, hash common.Hash, key *ecdsa.PrivateKey) error {
+func (s *SignatureHandler) VerifyFromHash(signature []byte, hash common.Hash, address string) error {
 	if signature[crypto.RecoveryIDOffset] == 27 || signature[crypto.RecoveryIDOffset] == 28 {
 		signature[crypto.RecoveryIDOffset] -= 27 // Transform yellow paper V from 27/28 to 0/1
 	}
@@ -59,30 +78,25 @@ func (s *SignatureHandler) VerifyFromHash(signature []byte, hash common.Hash, ke
 	if verErr != nil {
 		return verErr
 	}
-	publicKey := key.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		return errors.New("error getting public key from private key")
-	}
-
-	pubKeyStr := crypto.PubkeyToAddress(*publicKeyECDSA).String()
-	if pubKeyStr != crypto.PubkeyToAddress(*sigPublicKeyECDSA).String() {
+	recoveredAddress := crypto.PubkeyToAddress(*sigPublicKeyECDSA).String()
+	if address != recoveredAddress {
+		fmt.Printf("address input: %s\n, address recovered: %s\n", address, recoveredAddress)
 		return errors.New("signature public key verification failure")
 	}
 	return nil
 }
 
 func (s *SignatureHandler) GenerateKey() (privateKey, address string, err error) {
-	key, err := crypto.GenerateKey()
-	if err != nil {
-		return "", "", err
+	key, genErr := crypto.GenerateKey()
+	if genErr != nil {
+		return "", "", genErr
 	}
 	privateKeyStr := hexutil.Encode(crypto.FromECDSA(key))[2:]
-	address, addrErr := s.PrivateKeyToAddress(privateKeyStr)
+	addr, addrErr := s.PrivateKeyToAddress(privateKeyStr)
 	if addrErr != nil {
 		return "", "", addrErr
 	}
-	return privateKeyStr, address, nil
+	return privateKeyStr, addr, nil
 }
 
 func (s *SignatureHandler) PrivateKeyToAddress(privateKey string) (address string, err error) {
@@ -102,11 +116,15 @@ func (s *SignatureHandler) AddressToEIP55(address string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return validAddress.Original(), nil
+	return validAddress.Address().String(), nil
 }
 
 func (s *SignatureHandler) Verify(message, signature, address string) (err error) {
-	sig := hexutil.MustDecode(signature)
+	sig, decodeErr := hexutil.Decode(signature)
+	if decodeErr != nil {
+		fmt.Println("error decoding signature: " + signature)
+		return decodeErr
+	}
 	if sig[crypto.RecoveryIDOffset] == 27 || sig[crypto.RecoveryIDOffset] == 28 {
 		sig[crypto.RecoveryIDOffset] -= 27 // Transform yellow paper V from 27/28 to 0/1
 	}
@@ -119,20 +137,20 @@ func (s *SignatureHandler) Verify(message, signature, address string) (err error
 
 	sigAddress := crypto.PubkeyToAddress(*sigPublicKeyECDSA).Hex()
 	//Make sure the addresses are valid
-	validAddress, addressValidationErr := common.NewMixedcaseAddressFromString(sigAddress)
+	validAddress, addressValidationErr := s.AddressToEIP55(sigAddress)
 	if addressValidationErr != nil {
 		fmt.Println("sigAddress: " + addressValidationErr.Error())
 		fmt.Println(sigAddress)
 		return addressValidationErr
 	}
-	eventValidAddress, eventAddressValidationErr := common.NewMixedcaseAddressFromString(address)
+	eventValidAddress, eventAddressValidationErr := s.AddressToEIP55(address)
 	if eventAddressValidationErr != nil {
 		fmt.Println("givenAddress: " + eventAddressValidationErr.Error())
 		fmt.Println(address)
 		return eventAddressValidationErr
 	}
 
-	if validAddress.String() != eventValidAddress.String() {
+	if validAddress != eventValidAddress {
 		errMsg := fmt.Sprintf(
 			"\nhash generated: %s\n address given: %s\naddress retrieved: %s\nmessage: %s\nsignature: %s\n",
 			hash.Hex(), address, sigAddress, message, signature)

b9f679b0ad37afcaa89a58d54df6ec7acfd7f108
Ian <ian.moreno@artie.com>
2023-01-17 14:28:09 -0500

    Moving ENV Variables to config structs for /web and /api containers

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -1,5 +1,33 @@
 package main
 
-type Config struct {
+import (
+"contract-service/utils"
+"fmt"
+"strconv"
+)
 
+type Config struct {
+	TxnHost string
+	Port int
+}
+
+func NewConfig() (Config, error) {
+	var port, txnHost string
+	envErr := utils.GetEnvVarBatch([]string{"PORT", "TXN_HOST"}, &port, &txnHost)
+	if envErr != nil {
+		return Config{}, envErr
+	}
+	prt, convErr := strconv.Atoi(port)
+	if convErr != nil {
+		return Config{}, convErr
+	}
+	return Config{
+		TxnHost: txnHost,
+		Port:    prt,
+	}, nil
+}
+
+func (c *Config) String()string {
+	return fmt.Sprintf("{\n\tPort: %d\n\tTxnHost: %s}\n",
+		c.Port, c.TxnHost)
 }

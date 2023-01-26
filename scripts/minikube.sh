d28e8aef465b40ca1c6082057b57e1865b056d02
Ian <ian.moreno@artie.com>
2023-01-26 17:20:54 -0500

    initializing helm charts, setting up push to ecr script and setting up minikube deployment script for local development

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
+OLDCONTEXT=(kubectl config current-context)
+kubectl config use-context docker-desktop
 minikube start
 eval $(minikube docker-env)
 
 ./scripts/build.sh
 
-docker build -f ./build/generator/Dockerfile -t "generator" .
+docker build -f ./build/signer/Dockerfile -t "signer" .
+docker build -f ./build/contract-manager/Dockerfile -t "contract-manager" .
+docker build -f ./build/transaction-manager/Dockerfile -t "transaction-manager" .
+docker build -f ./build/api/Dockerfile -t "contract-api" .
+docker build -f ./build/web/Dockerfile -t "contract-web" .
 
-docker build -f ./build/pong/Dockerfile -t "pong" .
-
-docker build -f ./build/artieverseAvatarService/Dockerfile -t "artieverse_avatar_service" .
-
-
-
-minikube image load pong --daemon --overwrite
-minikube image load generator --daemon --overwrite
-minikube image load artieverse_avatar_service --daemon --overwrite
+minikube image load signer --daemon --overwrite
+minikube image load contract-manager --daemon --overwrite
+minikube image load transaction-manager --daemon --overwrite
+minikube image load contract-api --daemon --overwrite
+minikube image load contract-web --daemon --overwrite
 
 helm repo update
 
 kubectl create namespace pong-dev
-kubectl create namespace projectcontour
 
-helm install cert-manager jetstack/cert-manager -n cert-manager --create-namespace --version v1.3.1 --set installCRDs=true
-helm install my-release -n agones-system agones/agones --version "1.18" --set "gameservers.namespaces={default,pong-dev}" --create-namespace --set "agones.image.sdk.tag=1.18.0-70d56ad-linux_amd64"
-helm install linkerd2 --set-file identityTrustAnchorsPEM=ca.crt --set-file identity.issuer.tls.crtPEM=issuer.crt --set-file identity.issuer.tls.keyPEM=issuer.key --set identity.issuer.crtExpiry=$(date -v+8760H +"%Y-%m-%dT%H:%M:%SZ") linkerd/linkerd2
-helm install contour -f ./helm/pong/values.yaml bitnami/contour -n projectcontour --set installCRDs=true
+helm install nats nats/nats -n pong-dev
 sleep 3
-helm install pong -f ./helm/pong/values-dev.yaml ./helm/pong -n pong-dev
-helm install artieverse-avatar-service -f ./helm/artieverseAvatarService/values.yaml ./helm/artieverseAvatarService -n artieverse-avatar-service --create-namespace
-helm install generator -f ./helm/generator/values.yaml ./helm/generator -n generator --create-namespace
+helm install signer -f ./helm/signer/values-dev.yaml ./helm/signer -n pong-dev
+helm install contract-manager -f ./helm/contract-manager/values-dev.yaml ./helm/contract-manager -n pong-dev
+helm install transaction-manager -f ./helm/transaction-manager/values-dev.yaml ./helm/transaction-manager -n pong-dev
+helm install contract-api -f ./helm/api/values-dev.yaml ./helm/api -n pong-dev
+helm install contract-web -f ./helm/api/values-dev.yaml ./helm/contract-web -n pong-dev
+
+kubectl config use-context "$OLDCONTEXT"

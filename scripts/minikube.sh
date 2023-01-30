0c0392ea179df7cff817583ed96aa89c4a7014d6
Ian <ian.moreno@artie.com>
2023-01-30 18:18:18 -0500

    minikube.sh deployment is functioning, all services are being deployed to minikube and all ports are opened on localhost

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
+./scripts/build_tag_docker.sh
+
 OLDCONTEXT=$(kubectl config current-context)
-kubectl config use-context docker-desktop
+minikube delete && minikube start
+eval $(minikube docker-env)
+kubectl config use-context minikube
 echo -n "Using the kubernetes context:"
 kubectl config current-context
 echo -n "....... "
 sleep 5
 echo "starting."
-minikube delete && minikube start
-eval $(minikube docker-env)
-
-./scripts/build.sh
-
-docker build -f ./build/signer/Dockerfile -t "signer" .
-docker build -f ./build/contract-manager/Dockerfile -t "contract-manager" .
-docker build -f ./build/transaction-manager/Dockerfile -t "transaction-manager" .
-docker build -f ./build/api/Dockerfile -t "contract-api" .
-docker build -f ./build/web/Dockerfile -t "contract-web" .
 
 minikube image load signer --daemon --overwrite
 minikube image load contract-manager --daemon --overwrite
@@ -28,18 +22,24 @@ helm repo update
 
 kubectl create namespace pong-dev
 
+
+helm install contour bitnami/contour -n pong-dev --set installCRDs=true
 helm install dynamodb -f ./helm/dynamodb/values.yaml ./helm/dynamodb -n pong-dev
 sleep 3
 helm install signer -f ./helm/signer/values-dev.yaml ./helm/signer -n pong-dev
 helm install contract-manager -f ./helm/contract-manager/values-dev.yaml ./helm/contract-manager -n pong-dev
 helm install transaction-manager -f ./helm/transaction-manager/values-dev.yaml ./helm/transaction-manager -n pong-dev
 helm install contract-api -f ./helm/api/values-dev.yaml ./helm/api -n pong-dev
-helm install contract-web -f ./helm/api/values-dev.yaml ./helm/web -n pong-dev
+helm install contract-web -f ./helm/web/values-dev.yaml ./helm/web -n pong-dev
 
-kubectl port-forward service/signer 8081:8081
-kubectl port-forward service/contract-manager 8082:8082
-kubectl port-forward service/transaction-manager 8083:8083
-kubectl port-forward service/contract-web 8084:8084
-kubectl port-forward service/contract-api 8085:8085
+
+echo "Opening ports..."
+sleep 10
+
+kubectl port-forward service/signer 8081:8081 -n pong-dev &
+kubectl port-forward service/contract-manager 8082:8082 -n pong-dev &
+kubectl port-forward service/transaction-manager 8083:8083 -n pong-dev &
+kubectl port-forward service/contract-web 8084:8084 -n pong-dev &
+kubectl port-forward service/contract-api 8085:8085 -n pong-dev &
 
 kubectl config use-context "$OLDCONTEXT"
